7a41fe1177c54d82ca387af35c5c9f666889f6e4
Scott King <kingsc850@protonmail.com>
2020-12-03 20:37:38 -0500

    Fix mobile image spacing

--- src/Image.js
+++ src/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Image = props => {
   return (
-    <div className="image-container w-5/6 md:w-3/5 my-3 md:my-6">
+    <div className="image-container w-5/6 md:w-3/5 my-6 md:my-12">
       <img src={props.src} alt="" />
     </div>
   );
