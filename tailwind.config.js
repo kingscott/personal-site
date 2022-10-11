045eb74688b805905ccd2c0918a22d8c571fd7b9
Scott King <me@kingscott.ca>
2022-10-11 18:29:12 -0400

    Update photo gallery to use next.js
    Fix double quotes and minor formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    screens: {      
+      'xs': '574px',
+      ...defaultTheme.screens,      
+      '3xl': '1792px',
+      '4xl': '2048px',
+    },
   },
   plugins: [],
-};
+}
