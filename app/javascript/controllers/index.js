9a0fac124223810b6c1985761723dcac29c0f00e
kingscott <me@kingscott.ca>
2023-11-17 15:50:28 -0500

    Add mobile menu functionality
    Fix transition and fix logo

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,9 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+import MenuController from './menu_controller'
+application.register("menu", MenuController)
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
